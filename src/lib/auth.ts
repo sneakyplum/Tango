859d03fdd82ba59ef606596bee80b4a3c78bd4cb
Playing - pokemon go Xbox one edition <linkolnkrauskopf@gmail.com>
2026-08-13 00:29:53 -0600

    stupid little backtick error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,7 +10,7 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
-    baseURL: "${process.env.BETTER_AUTH_URL}",
+    baseURL: `${process.env.BETTER_AUTH_URL}!`,
     emailAndPassword: {    
         enabled: true
     },
