73d39414efa09e886e382d517283f0ac9760ad69
Playing - pokemon go Xbox one edition <linkolnkrauskopf@gmail.com>
2026-08-13 09:43:25 -0600

    changed env variables

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { genericOAuthClient } from "better-auth/client/plugins"
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: `${process.env.BETTER_AUTH_URL}`,
+    baseURL: process.env.BETTER_AUTH_URL!,
     plugins: [
         genericOAuthClient()
     ]
